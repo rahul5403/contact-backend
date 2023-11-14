6e93cad0058890c3f3a042b42fbaeb9d74530155
rahul5403 <rahul54meshram@gmail.com>
2023-11-14 22:49:12 +0530

    First commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const {registerUser, currentUser, loginUser} = require("../controllers/userControllers");
+const validateToken = require("../middleware/validateTokenHandler");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/current",validateToken,currentUser);
+
+module.exports = router;
